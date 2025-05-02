8b452e28f118108224d6fdfbc3fa3fa044d60f0f
bigmadrussian <88528631+bigmadrussian@users.noreply.github.com>
2025-05-02 23:00:43 +0300

    Update DatabaseHelper.java

--- src/main/java/org/example/DatabaseHelper.java
+++ src/main/java/org/example/DatabaseHelper.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class DatabaseHelper {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "mix030897";
+    private static final String PASSWORD = "пароль";
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
